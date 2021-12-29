549515a83a9dd95b1977757b765f9157aec5e5f0
Gabriel Rocha <contate.gabrielr@gmail.com>
2021-12-29 15:58:38 -0300

    Created UI and ChessMatch

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,9 +1,12 @@
 package applications;
 
+import chess.ChessMatch;
+
 public class Program {
     public static void main(String[] args) {
 
-
+        ChessMatch chessMatch = new ChessMatch();
+        UserInterface.printBoard(chessMatch.getPieces());
 
 
     }
